592066bc41a8dc9c7cda5cf1d81f0385558d3419
Mathew Poff <matp@live.ca>
2022-03-22 15:42:58 -0400

    Chakra ui initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <ColorModeProvider options={{ initialColorMode: "dark" }}>
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
