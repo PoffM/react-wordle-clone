5ed9329dc29342c72104136852829dd99522fc17
Mathew Poff <matp@live.ca>
2022-03-25 15:11:02 -0400

    Added horizontal shake animation when there is a guessing error (invalid word or not enough letters).

--- src/components/LetterGrid.tsx
+++ src/components/LetterGrid.tsx
@@ -1,5 +1,7 @@
 import { AspectRatio, Box, HStack, VStack } from "@chakra-ui/react";
+import { motion, useAnimation } from "framer-motion";
 import { range } from "lodash";
+import { ComponentProps, useEffect } from "react";
 import { WordleState } from "../hooks/useWordleState";
 
 export interface LetterGridProps {
@@ -7,37 +9,70 @@ export interface LetterGridProps {
 }
 
 export function LetterGrid({ wordleState }: LetterGridProps) {
-  const { rowData } = useLetterGridData({ wordleState });
+  const { rows } = useLetterGridData({ wordleState });
 
   return (
     <VStack width="100%" maxW="21rem">
-      {rowData.map(({ columnData }, rowNum) => (
-        <HStack flex={1} width="100%" key={rowNum}>
-          {columnData.map(({ bgColor, letter }, colNum) => (
-            <AspectRatio key={colNum} flex={1} ratio={1}>
-              <Box
-                key={colNum}
-                border="1px solid"
-                bg={bgColor}
-                userSelect="none"
-                fontWeight="bold"
-                fontSize="2rem"
-              >
-                {letter}
-              </Box>
-            </AspectRatio>
-          ))}
-        </HStack>
+      {rows.map((rowData, rowNum) => (
+        <LetterGridRow {...rowData} key={rowNum} />
       ))}
     </VStack>
   );
 }
 
-function useLetterGridData({ wordleState }: LetterGridProps) {
-  const { maxGuesses, wordLength, currentGuess, submittedGuesses, solution } =
-    wordleState;
+interface LetterGridRowData {
+  columnData: {
+    bgColor: string | undefined;
+    letter: string | undefined;
+  }[];
+  rowError: { message: string } | null;
+}
 
-  const rowData = range(0, maxGuesses).map((rowNum) => {
+const MotionHStack = motion<ComponentProps<typeof HStack>>(HStack);
+
+function LetterGridRow({ columnData, rowError }: LetterGridRowData) {
+  // Shake horizontally when there is a new error:
+  const controls = useAnimation();
+  useEffect(() => {
+    if (rowError) {
+      void controls.start({
+        translateX: [0, -1, 2, -4, 4, -4, 4, -4, 2, -1, 0],
+        transition: { duration: 0.6 },
+      });
+    }
+  }, [controls, rowError]);
+
+  return (
+    <MotionHStack animate={controls} flex={1} width="100%">
+      {columnData.map(({ bgColor, letter }, colNum) => (
+        <AspectRatio key={colNum} flex={1} ratio={1}>
+          <Box
+            key={colNum}
+            border="1px solid"
+            bg={bgColor}
+            userSelect="none"
+            fontWeight="bold"
+            fontSize="2rem"
+          >
+            {letter}
+          </Box>
+        </AspectRatio>
+      ))}
+    </MotionHStack>
+  );
+}
+
+function useLetterGridData({ wordleState }: LetterGridProps) {
+  const {
+    maxGuesses,
+    wordLength,
+    currentGuess,
+    submittedGuesses,
+    solution,
+    currentGuessError,
+  } = wordleState;
+
+  const rows = range(0, maxGuesses).map((rowNum) => {
     const isCurrentGuess = rowNum === submittedGuesses.length;
     const isSubmitted = Boolean(submittedGuesses[rowNum]);
     const rowGuess = isCurrentGuess ? currentGuess : submittedGuesses[rowNum];
@@ -46,7 +81,9 @@ function useLetterGridData({ wordleState }: LetterGridProps) {
       .filter((idx) => rowGuess?.[idx] !== solution[idx])
       .map((idx) => solution[idx]);
 
-    return {
+    const rowError = isCurrentGuess ? currentGuessError : null;
+
+    const rowData: LetterGridRowData = {
       columnData: range(0, wordLength).map((colNum) => {
         const letter = rowGuess?.charAt(colNum);
         const letterIsInRemainingLetters = Boolean(
@@ -66,8 +103,11 @@ function useLetterGridData({ wordleState }: LetterGridProps) {
 
         return { bgColor, letter };
       }),
+      rowError,
     };
+
+    return rowData;
   });
 
-  return { rowData };
+  return { rows };
 }
