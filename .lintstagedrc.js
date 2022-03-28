68dedd3cbc793df33320c3a2862fa5ea1c02fb29
Mathew Poff <matp@live.ca>
2022-03-28 11:31:45 -0400

    Disallowed warnings for lint-staged.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
+  `next lint --max-warnings=0 --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand, "prettier --write"],
+  "*.{ts,tsx}": [buildEslintCommand, "prettier --write"],
 };
